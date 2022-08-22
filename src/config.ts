54f3b1da4aab9d7f07b2f71fc07cc1624c49c16f
CodeAwareness <codeawareness@gmail.com>
2022-08-22 07:30:05 -0700

    restoring local API dev

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export default {
   MAX_USER_NAME_LENGTH: 14,
   CODEAWARENESS: LOCAL ? `https://localhost:${PORT_DEV}` : 'https://codeawareness.com',
   EXT_SERVER: LOCAL ? `https://localhost:${PORT_DEV}` : 'https://codeawareness.com',
-  SERVER_WSS: LOCAL ? `wss://localhost:${PORT_DEV}` : 'wss://api.codeawareness.com/web',
-  SERVER_URL: 'https://api.codeawareness.com/v1',
+  SERVER_WSS: LOCAL ? `wss://localhost:${PORT_DEV}` : 'wss://vscode.codeawareness.com/web',
+  SERVER_URL: LOCAL ? `https://localhost:${PORT_DEV}/api/v1` : 'https://vscode.codeawareness.com/v1',
 }
