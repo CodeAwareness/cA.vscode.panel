c3ad458259acf431a4508843d55c73083ffd89b1
CodeAwareness <codeawareness@gmail.com>
2022-10-12 00:17:42 -0700

    fixed wss port

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 const PORT_DEV = 8885
+const PORT_WSS = 48408
 
 const LOCAL = !PRODUCTION
 
@@ -7,5 +8,5 @@ export default {
   MAX_USER_NAME_LENGTH: 14,
   EXT_SERVER: LOCAL ? `https://127.0.0.1:${PORT_DEV}` : 'https://vscode.codeawareness.com',
   SERVER_URL: LOCAL ? `https://127.0.0.1:${PORT_DEV}/api/v1` : 'https://api.codeawareness.com/v1',
-  SERVER_WSS: `wss://127.0.0.1:${PORT_DEV}`,
+  SERVER_WSS: `wss://127.0.0.1:${PORT_WSS}`,
 }
