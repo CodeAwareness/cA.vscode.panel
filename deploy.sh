1bd1af9bb11b390f61778fccad6f30579dc08485
CodeAwareness <codeawareness@gmail.com>
2022-10-18 02:08:52 -0700

    webpack creates a dist link nowadays... i will have to figure this one out too

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yarn build
+# yarn build
 cd dist
-tar czvf ../../vscode.panel.tgz .
+tar czvf /Users/markvasile/Code/CodeAwareness/vscode.panel.tgz .
 cd ..
 scp -i ~/.ssh/tokyo-dev.pem ../vscode.panel.tgz ec2-user@13.114.87.226:/var/w3/vscode
 ssh -i ~/.ssh/tokyo-dev.pem ec2-user@13.114.87.226 'cd /var/w3/vscode; tar xzvf vscode.panel.tgz;'
